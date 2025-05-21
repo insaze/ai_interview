ca2a9f39967e37593010cb3c8050d2270d4db309
itookyourboo <wignorbo@yandex.ru>
2025-05-21 21:00:22 +0300

    Fix test

--- backend/tests/test_parse_vacancy.py
+++ backend/tests/test_parse_vacancy.py
@@ -5,7 +5,7 @@ from app.parse_hh import parse_vacancy
 
 class TestParseVacancy(TestCase):
     def test_parse_vacancy(self):
-        url = 'https://spb.hh.ru/vacancy/118824674?from=anonymous_recommended&hhtmFrom=main'
+        url = 'https://spb.hh.ru/vacancy/119319270'
         vacancy_info = parse_vacancy(url)
         self.assertEqual(vacancy_info.title, 'Backend-разработчик (Леста Старт)')
         self.assertEqual(vacancy_info.experience, 'не требуется')
