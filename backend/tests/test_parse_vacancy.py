1c8fab3926e533e08858a90263e052d6da3fec89
itookyourboo <wignorbo@yandex.ru>
2025-05-21 21:18:48 +0300

    Fix test and CI

--- backend/tests/test_parse_vacancy.py
+++ backend/tests/test_parse_vacancy.py
@@ -7,7 +7,7 @@ class TestParseVacancy(TestCase):
     def test_parse_vacancy(self):
         url = 'https://spb.hh.ru/vacancy/119319270'
         vacancy_info = parse_vacancy(url)
-        self.assertEqual(vacancy_info.title, 'Backend-разработчик (Леста Старт)')
-        self.assertEqual(vacancy_info.experience, 'не требуется')
+        self.assertEqual(vacancy_info.title, 'Python-разработчик (Middle/ Senior)')
+        self.assertEqual(vacancy_info.experience, '3–6 лет')
         self.assertGreater(len(vacancy_info.description), 100)
-        self.assertEqual(vacancy_info.company, 'Леста Игры')
+        self.assertEqual(vacancy_info.company, 'Lenvendo')
