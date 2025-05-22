e74731033a61d5a16050cccc1d86e6cfbee29ee0
itookyourboo <wignorbo@yandex.ru>
2025-05-22 12:15:53 +0300

    Fix tests and move sonar up

--- backend/tests/test_parse_vacancy.py
+++ backend/tests/test_parse_vacancy.py
@@ -5,9 +5,9 @@ from app.parse_hh import parse_vacancy
 
 class TestParseVacancy(TestCase):
     def test_parse_vacancy(self):
-        url = 'https://spb.hh.ru/vacancy/118824674?from=anonymous_recommended&hhtmFrom=main'
+        url = 'https://spb.hh.ru/vacancy/119319270'
         vacancy_info = parse_vacancy(url)
-        self.assertEqual(vacancy_info.title, 'Backend-разработчик (Леста Старт)')
-        self.assertEqual(vacancy_info.experience, 'не требуется')
+        self.assertEqual(vacancy_info.title, 'Python-разработчик (Middle/ Senior)')
+        self.assertEqual(vacancy_info.experience, '3–6 лет')
         self.assertGreater(len(vacancy_info.description), 100)
-        self.assertEqual(vacancy_info.company, 'Леста Игры')
+        self.assertEqual(vacancy_info.company, 'Lenvendo')
