265a0b1a0dc4ab10c22293f3c4f33491ff1e611c
itookyourboo <wignorbo@yandex.ru>
2025-06-05 09:43:08 +0300

    Add test

--- backend/tests/test_parse_hh.py
+++ backend/tests/test_parse_hh.py
@@ -2,10 +2,11 @@ from pathlib import Path
 from unittest import TestCase
 from unittest.mock import patch
 
-from app.parse_hh import parse_vacancy
+import requests
+from app.parse_hh import _validate_response, parse_vacancy
 
 
-class TestParseVacancy(TestCase):
+class TestParseHH(TestCase):
     def test_parse_vacancy(self):
         vacancy_html = (Path(__file__).parent / 'mocks' / 'vacancy.html').read_text()
         with patch('app.parse_hh.get_html', return_value=vacancy_html):
@@ -15,3 +16,19 @@ class TestParseVacancy(TestCase):
             self.assertEqual(vacancy_info.experience, '3–6 лет')
             self.assertGreater(len(vacancy_info.description), 100)
             self.assertEqual(vacancy_info.company, 'ООО Карвиль')
+
+    def test_validate_response(self):
+        class Mock:
+            def __init__(self, ok: bool, text: str):
+                self.ok = ok
+                self.text = text
+
+        ok_response = Mock(True, 'OK')
+        try:
+            _validate_response(ok_response)
+        except:
+            self.fail()
+
+        err_response = Mock(False, 'Error')
+        with self.assertRaises(requests.exceptions.RequestException):
+            _validate_response(err_response)
